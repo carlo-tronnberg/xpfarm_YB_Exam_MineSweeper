0b3c8f67705feb0be921136194e625d5ede02be2
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-29 21:53:29 +0100

    chore: ✨ create dummy files
    Create dummy test and implementation files

---
+++ src/mineSweep_Exam.js
@@ -0,0 +1,17 @@
+class carlo_t_yb_MineSweep_Exam {
+  constructor() {
+    this.status = 'running';
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getValue(x) {
+    if (x == 1) return 'one';
+    return 'two';
+  }
+}
+
+module.exports = { carlo_t_yb_MineSweep_Exam };
+
---
+++ test/mineSweep_Exam.test.js
@@ -0,0 +1,21 @@
+const { carlo_t_yb_MineSweep_Exam } = require('../src/mineSweep_Exam');
+
+
+describe('carlo_t_yb_MineSweep_Exam', () => {
+  it('Start a new mineSweep_Exam', () => {
+    const mineSweep_Exam = new carlo_t_yb_MineSweep_Exam();
+
+    expect(mineSweep_Exam.getStatus()).toEqual('running');
+  });
+});
+
+describe('carlo_t_yb_MineSweep_Exam 2', () => {
+  it.each([
+    [1, 'one'],
+    [2, 'two'],
+  ])("Given ,      When %i,      Then  '%s'", (x, result) => {
+    const mineSweep_Exam = new carlo_t_yb_MineSweep_Exam();
+    expect(mineSweep_Exam.getValue(x)).toEqual(result);
+  });
+});
+
